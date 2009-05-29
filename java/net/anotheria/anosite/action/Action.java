bcb633e766c412d994a10e61720f7fd0ea888c70
Leon Rosenberg <leon.rosenberg@anotheria.net>
2009-05-29 09:34:56 +0000

    removed unneeded modifiers

--- java/net/anotheria/anosite/action/Action.java
+++ java/net/anotheria/anosite/action/Action.java
@@ -4,5 +4,5 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public interface Action {
-	public ActionCommand execute(HttpServletRequest req, HttpServletResponse responce, ActionMapping mapping) throws Exception;
+	ActionCommand execute(HttpServletRequest req, HttpServletResponse responce, ActionMapping mapping) throws Exception;
 }
