1fe7906cd1e8ed436c2689772b2048440444054f
Vitaliy Zhovtiuk <vzhovtiuk@anotheria.net>
2010-01-15 23:02:17 +0000

    action mapping

--- java/net/anotheria/anosite/handler/BoxHandlerWrapper.java
+++ java/net/anotheria/anosite/handler/BoxHandlerWrapper.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServletResponse;
 import net.anotheria.anosite.content.bean.BoxBean;
 import net.anotheria.anosite.gen.aswebdata.data.Box;
 import net.anotheria.asg.exception.ASGRuntimeException;
+import net.anotheria.maf.util.ModelObjectMapper;
 
 public class BoxHandlerWrapper implements BoxHandler{
 
@@ -25,6 +26,7 @@ public class BoxHandlerWrapper implements BoxHandler{
 
 	public BoxHandlerResponse submit(HttpServletRequest req,
 			HttpServletResponse res, Box box) throws ASGRuntimeException {
+		ModelObjectMapper.map(req, handler);
 		return producer.submit(req, res, box, handler);
 	}
 	
