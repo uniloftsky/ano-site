f38abd62112af793abc64aa072f80b8cd357ddfb
Vitaliy Zhovtiuk <vzhovtiuk@anotheria.net>
2010-02-25 22:35:57 +0000

    added ano-tags and RandomTag

---
+++ java/net/anotheria/anosite/tags/content/BaseResourceTag.java
@@ -0,0 +1,60 @@
+package net.anotheria.anosite.tags.content;
+
+import net.anotheria.anoprise.metafactory.MetaFactory;
+import net.anotheria.anoprise.metafactory.MetaFactoryException;
+import net.anotheria.anosite.gen.asresourcedata.data.TextResource;
+import net.anotheria.anosite.gen.asresourcedata.service.IASResourceDataService;
+import net.anotheria.asg.exception.ASGRuntimeException;
+import net.anotheria.tags.BaseTagSupport;
+import org.apache.log4j.Logger;
+
+import javax.servlet.jsp.JspException;
+import java.util.List;
+
+/**
+ * Base tag for handling resources in the jsp.
+ * @author lrosenberg
+ *
+ */
+public abstract class BaseResourceTag extends BaseTagSupport {
+	/**
+	 * Resource data service.
+	 */
+	private static IASResourceDataService service;
+	/**
+	 * log.
+	 */
+	private static Logger log = Logger.getLogger(BaseResourceTag.class);
+
+	/**
+	 * Init.
+	 */
+	static {
+		try {
+			service = MetaFactory.get(IASResourceDataService.class);
+		} catch (MetaFactoryException e) {
+			log.fatal("IASResourceDataService init failure",e);
+		}
+	}
+
+	protected static IASResourceDataService getResourceDataService(){
+		return service;
+	}
+	/**
+	 * Returns a text resource by its name.
+	 * @param key
+	 * @return
+	 * @throws JspException
+	 */
+	protected TextResource getTextResourceByName(String key) throws JspException{
+		try{
+			List<TextResource> resources = getResourceDataService().getTextResourcesByProperty(TextResource.PROP_NAME, key);
+			if (resources==null || resources.size()==0)
+				return null;
+			return resources.get(0);
+		}catch(ASGRuntimeException e){
+			log.error("getTextResourceByName("+key+")", e);
+			throw new JspException(e);
+		}
+	}
+}
---
+++ java/net/anotheria/anosite/tags/content/TextResourceTag.java
@@ -0,0 +1,59 @@
+package net.anotheria.anosite.tags.content;
+
+import javax.servlet.http.HttpSession;
+import javax.servlet.jsp.JspException;
+
+import net.anotheria.anosite.gen.asresourcedata.data.TextResource;
+import net.anotheria.anosite.util.AnositeConstants;
+
+/**
+ * Writes out the content of the resource. Writes out a link to the resource in edit mode.
+ * @author another
+ *
+ */
+public class TextResourceTag extends BaseResourceTag{
+
+	/**
+	 * Direct reference to resource key (name).
+	 */
+	private String key;
+
+	@Override public int doEndTag() throws JspException {
+
+		String myKey = null;
+		if (key!=null){
+			myKey = key;
+		}else{
+			myKey = (String)lookup();
+		}
+
+		boolean editable = false;
+		String txt = null;
+		TextResource resource = getTextResourceByName(myKey);
+		if (resource==null)
+			txt = "Missing key: "+myKey;
+		else
+			txt = resource.getValue();
+
+		HttpSession session = pageContext.getSession();
+		if (session!=null && resource!=null && session.getAttribute(AnositeConstants.SA_EDIT_MODE_FLAG)!=null)
+			editable = true;
+
+		if (editable){
+			String link = "<a href="+quote("cms/textresourceEdit?ts="+System.currentTimeMillis()+"&pId="+resource.getId())+" target="+quote("_blank")+">E</a>&nbsp;";
+			write(link);
+		}
+		write(txt);
+
+
+		return SKIP_BODY;
+	}
+
+	public String getKey() {
+		return key;
+	}
+
+	public void setKey(String key) {
+		this.key = key;
+	}
+}
