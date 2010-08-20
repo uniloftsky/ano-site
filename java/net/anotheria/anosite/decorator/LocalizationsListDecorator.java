9c84a5a26887a27d5ce34608ef625c9de4b75df2
Denis Metelin <denis.metelin@nazax.com>
2010-08-20 11:12:05 +0000

    Generator: Localization

---
+++ java/net/anotheria/anosite/decorator/LocalizationsListDecorator.java
@@ -0,0 +1,39 @@
+package net.anotheria.anosite.decorator;
+
+import net.anotheria.anoprise.metafactory.MetaFactory;
+import net.anotheria.anoprise.metafactory.MetaFactoryException;
+import net.anotheria.anosite.gen.assitedata.service.ASSiteDataServiceFactory;
+import net.anotheria.anosite.gen.assitedata.service.IASSiteDataService;
+import net.anotheria.asg.exception.ASGRuntimeException;
+import org.apache.log4j.Logger;
+
+
+/**
+ * Decorator for linked scripts.
+ * @author denis
+ */
+public class LocalizationsListDecorator extends LinksListDecorator{
+	
+	/**
+	 * As web data service for MediaLinks retrieval.
+	 */
+	private static IASSiteDataService service;
+
+	/**
+	 * Init.
+	 */
+	static {
+		try {
+			service = MetaFactory.get(IASSiteDataService.class);
+		} catch (MetaFactoryException e) {
+          Logger.getLogger(LocalizationsListDecorator.class).fatal("IASSiteDataService  init failure",e);
+		}
+	}
+
+	@Override
+	protected String getLinkTargetName(String targetId) throws ASGRuntimeException {
+		return service.getMediaLink(targetId).getName();
+	}
+	
+
+}
--- java/net/anotheria/anosite/decorator/MediaLinksListDecorator.java
+++ java/net/anotheria/anosite/decorator/MediaLinksListDecorator.java
@@ -2,37 +2,37 @@ package net.anotheria.anosite.decorator;
 
 import net.anotheria.anoprise.metafactory.MetaFactory;
 import net.anotheria.anoprise.metafactory.MetaFactoryException;
-import net.anotheria.anosite.gen.assitedata.service.ASSiteDataServiceFactory;
-import net.anotheria.anosite.gen.assitedata.service.IASSiteDataService;
+import net.anotheria.anosite.gen.asresourcedata.service.IASResourceDataService;
 import net.anotheria.asg.exception.ASGRuntimeException;
+
 import org.apache.log4j.Logger;
 
 
 /**
- * Decorator for linked scripts.
+ * Decorator for linked localizations.
  * @author denis
  */
 public class MediaLinksListDecorator extends LinksListDecorator{
 	
 	/**
-	 * As web data service for MediaLinks retrieval.
+	 * As web data service for LocalizationBundles retrieval.
 	 */
-	private static IASSiteDataService service;
+	private static IASResourceDataService service;
 
 	/**
 	 * Init.
 	 */
 	static {
 		try {
-			service = MetaFactory.get(IASSiteDataService.class);
+			service = MetaFactory.get(IASResourceDataService.class);
 		} catch (MetaFactoryException e) {
-          Logger.getLogger(MediaLinksListDecorator.class).fatal("IASSiteDataService  init failure",e);
+          Logger.getLogger(MediaLinksListDecorator.class).fatal("IASResourceDataService  init failure",e);
 		}
 	}
 
 	@Override
 	protected String getLinkTargetName(String targetId) throws ASGRuntimeException {
-		return service.getMediaLink(targetId).getName();
+		return service.getLocalizationBundle(targetId).getName();
 	}
 	
 
