8723e37e9342219836eeba0d6ea4fa62d0a4f1f4
Dmitriy Zhmud <dmitriy.zhmud@anotheria.net>
2011-03-25 17:44:40 +0000

    changes in order to provide tooltip descriptions to fields in CMS dialogs

---
+++ html/cms_static/js/cms-tooltip.js
@@ -0,0 +1,15 @@
+$(function() {
+	$('.showTooltip').live({
+	mouseover: function() {
+		var text = '<div class="tipText">' + $(this).text() + '</div>';
+		$(this).after(text);
+		if ($(this).position().left - $(this).next('.tipText').width() < 0) {
+			$(this).next('.tipText').css('top', $(this).position().top +'px').css('left', $(this).position().left+17+'px').css('margin-top', -$(this).next('.tipText').height()-15+'px').hide().fadeIn('fast');
+		} else {
+			$(this).next('.tipText').css('top', $(this).position().top +'px').css('left', $(this).position().left-$(this).next('.tipText').width()-14+'px').css('margin-top', -$(this).next('.tipText').height()-15+'px').hide().fadeIn('fast');
+		}
+	},
+	mouseout: function() {
+		$(this).next('.tipText').hide().remove();
+	}})
+});
