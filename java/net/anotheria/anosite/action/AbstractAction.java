50f113773b70e64972e58ab9481b34fd64d1e81f
Leon Rosenberg <leon.rosenberg@anotheria.net>
2011-08-06 23:18:43 +0000

    test commit

--- java/net/anotheria/anosite/action/AbstractAction.java
+++ java/net/anotheria/anosite/action/AbstractAction.java
@@ -4,6 +4,12 @@ import net.anotheria.anoplass.api.APICallContext;
 import net.anotheria.anoplass.api.session.APISessionImpl;
 
 public abstract class AbstractAction implements Action{
+	
+	/**
+	 * no comment.
+	 * @param name
+	 * @param attribute
+	 */
 	protected void sendAttributeToPage(String name, Object attribute){
 		((APISessionImpl)APICallContext.getCallContext().getCurrentSession()).addAttributeToActionScope(name, attribute);
 	}
