76b1952b36798b018e672b4d71bc70da77f4bfc6
Alexandr Osadchy <aosadchy@anotheria.net>
2012-02-06 16:12:16 +0000

    fixed constant naming.

--- java/net/anotheria/anosite/wizard/handler/BaseWizardHandler.java
+++ java/net/anotheria/anosite/wizard/handler/BaseWizardHandler.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import net.anotheria.anoplass.api.APIFinder;
 import net.anotheria.anosite.gen.aswizarddata.data.WizardDef;
+import static net.anotheria.anosite.handler.validation.AbstractValidationBoxHandler.REQ_PARAM_VALIDATION_ONLY;
 import net.anotheria.anosite.wizard.WizardCommand;
 import net.anotheria.anosite.wizard.api.WizardAO;
 import net.anotheria.anosite.wizard.api.WizardAPI;
@@ -109,8 +110,7 @@ public class BaseWizardHandler implements WizardHandler {
 	 * @return boolean value
 	 */
 	protected boolean isNextStepAllowed(HttpServletRequest req, WizardAO wizard, WizardStepAO step, boolean isSubmit) {
-		//TODO :  "isAjaxValidation"  change  to  validationHandler  constant after it  replace!
-		return req.getParameter("isAjaxValidation") == null && isSubmit;
+		return req.getParameter(REQ_PARAM_VALIDATION_ONLY) == null && isSubmit;
 	}
 
 
@@ -138,8 +138,7 @@ public class BaseWizardHandler implements WizardHandler {
 	 * @return boolean value
 	 */
 	protected boolean isFinishAllowed(HttpServletRequest req, WizardAO wizard, WizardStepAO step, boolean isSubmit) {
-		//TODO :  "isAjaxValidation"  change  to  validationHandler  constant after it  replace!
-		return req.getParameter("isAjaxValidation") == null && isSubmit;
+		return req.getParameter(REQ_PARAM_VALIDATION_ONLY) == null && isSubmit;
 	}
 
 	/**
