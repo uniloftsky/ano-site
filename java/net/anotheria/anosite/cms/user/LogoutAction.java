65859b40fe2bac3e62ea01e354e074842e28233a
Vlad Bezuhlyi <vbezuhlyi@anotheria.net>
2012-07-17 15:01:18 +0000

    Some classes and ChangePage.jsp was moved to net.anotheria.anosite.cms subpackages.

---
+++ java/net/anotheria/anosite/cms/user/LogoutAction.java
@@ -0,0 +1,25 @@
+package net.anotheria.anosite.users;
+
+import net.anotheria.maf.action.ActionForward;
+import net.anotheria.maf.action.ActionMapping;
+import net.anotheria.maf.bean.FormBean;
+import net.anotheria.webutils.actions.AccessControlMafAction;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class LogoutAction extends AccessControlMafAction {
+
+    @Override
+    public ActionForward execute(ActionMapping mapping, FormBean af, HttpServletRequest req, HttpServletResponse res) throws Exception {
+        removeBeanFromSession(req, BEAN_USER_ID);
+        res.addCookie(createAuthCookie(req, "CAFE", "BABE"));
+        // res.sendRedirect(req.getContextPath());
+        // return null;
+        return mapping.findForward("success");
+    }
+
+
+}
+ 
