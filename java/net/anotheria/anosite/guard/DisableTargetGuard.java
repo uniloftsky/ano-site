00d76dc2e853e004d26924bfe9bb8a806b87025c
Vlad Lukjanenko <lukjanenko99@gmail.com>
2016-02-17 15:49:30 +0200

    MAT-2031 Migrate DONTSHOW guard to core cms

---
+++ java/net/anotheria/anosite/guard/DisableTargetGuard.java
@@ -0,0 +1,17 @@
+package net.anotheria.anosite.guard;
+
+import net.anotheria.asg.data.DataObject;
+import net.anotheria.asg.exception.ASGRuntimeException;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @auther Vlad Lukjanenko
+ */
+public class DisableTargetGuard implements ConditionalGuard {
+
+    @Override
+    public boolean isConditionFullfilled(DataObject object, HttpServletRequest req) throws ASGRuntimeException {
+        return false;
+    }
+}
