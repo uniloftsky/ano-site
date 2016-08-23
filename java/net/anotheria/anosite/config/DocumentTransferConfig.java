a6be6bd0f46b26a328cc46f91a95bc3c22444dfe
Vlad Lukjanenko <lukjanenko99@gmail.com>
2016-08-23 14:35:29 +0300

    Target prod url configurator

---
+++ java/net/anotheria/anosite/config/DocumentTransferConfig.java
@@ -0,0 +1,66 @@
+package net.anotheria.anosite.config;
+
+import org.configureme.ConfigurationManager;
+import org.configureme.annotations.Configure;
+import org.configureme.annotations.ConfigureMe;
+import org.configureme.annotations.DontConfigure;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.Serializable;
+
+/**
+ * Gets domain name we
+ *
+ * @author Vlad Lukjanenko
+ */
+@ConfigureMe(name = "ano-site-document-transfer-config")
+public final class DocumentTransferConfig implements Serializable {
+
+    /**
+     * Logger.
+     */
+    @DontConfigure
+    private static final Logger LOGGER = LoggerFactory.getLogger(DocumentTransferConfig.class);
+
+    /**
+     * Instance.
+     * */
+    private static DocumentTransferConfig INSTANCE;
+
+    /**
+     * Name of target point we need send document to.
+     * */
+    @Configure
+    private String domain;
+
+
+    private DocumentTransferConfig() {
+
+        try {
+            ConfigurationManager.INSTANCE.configure(this);
+        } catch (final IllegalArgumentException e) {
+            LOGGER.warn("Configuration fail[" + e.getMessage() + "]. Relaying on defaults.");
+        }
+    }
+
+
+    public static DocumentTransferConfig getInstance() {
+
+        if (INSTANCE == null) {
+            synchronized (DocumentTransferConfig.class) {
+                INSTANCE = new DocumentTransferConfig();
+            }
+        }
+
+        return INSTANCE;
+    }
+
+    public String getDomain() {
+        return domain;
+    }
+
+    public void setDomain(String domain) {
+        this.domain = domain;
+    }
+}
