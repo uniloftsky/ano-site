cbf472426e18be8bbf83ede26166cebdd1c1910a
HELLnyk <yurock.ua@gmail.com>
2019-10-08 16:02:15 +0300

    fixed jersey client config for https

--- java/net/anotheria/anosite/util/staticutil/JerseyClientUtil.java
+++ java/net/anotheria/anosite/util/staticutil/JerseyClientUtil.java
@@ -2,6 +2,18 @@ package net.anotheria.anosite.util.staticutil;
 
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.config.ClientConfig;
+import com.sun.jersey.api.client.config.DefaultClientConfig;
+import com.sun.jersey.client.urlconnection.HTTPSProperties;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 
 /**
  * Util for configuring jersey client.
@@ -26,12 +38,51 @@ public final class JerseyClientUtil {
     private static final Client CLIENT;
 
     /**
+     * {@link TrustManager} config.
+     */
+    private static final TrustManager[] certs = new TrustManager[]{
+            new X509TrustManager() {
+                @Override
+                public X509Certificate[] getAcceptedIssuers() {
+                    return null;
+                }
+
+                @Override
+                public void checkServerTrusted(X509Certificate[] chain, String authType)
+                        throws CertificateException {
+                }
+
+                @Override
+                public void checkClientTrusted(X509Certificate[] chain, String authType)
+                        throws CertificateException {
+                }
+            }
+    };
+
+    /**
      * Configure jersey client
      */
     static {
-        CLIENT = Client.create();
-        CLIENT.getProperties().put(ClientConfig.PROPERTY_CONNECT_TIMEOUT, CONNECT_TIMEOUT);
-        CLIENT.getProperties().put(ClientConfig.PROPERTY_READ_TIMEOUT, READ_TIMEOUT);
+
+        HostnameVerifier hostnameVerifier = HttpsURLConnection.getDefaultHostnameVerifier();
+        ClientConfig config = new DefaultClientConfig();
+        SSLContext ctx = null;
+        try {
+            ctx = SSLContext.getInstance("SSL");
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("Unable to get SSL context. " + e.getMessage());
+        }
+        try {
+            ctx.init(null, certs, null);
+        } catch (KeyManagementException e) {
+            throw new RuntimeException("Unable to init SSL context. " + e.getMessage());
+        }
+
+        config.getProperties().put(HTTPSProperties.PROPERTY_HTTPS_PROPERTIES, new HTTPSProperties(hostnameVerifier, ctx));
+        config.getProperties().put(ClientConfig.PROPERTY_CONNECT_TIMEOUT, CONNECT_TIMEOUT);
+        config.getProperties().put(ClientConfig.PROPERTY_READ_TIMEOUT, READ_TIMEOUT);
+
+        CLIENT = Client.create(config);
     }
 
     /**
