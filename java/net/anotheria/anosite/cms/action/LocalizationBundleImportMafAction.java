998834ce66d57266db4dc53c8c209c25c5a76c5b
asamoilich <asamoilich@anotheria.net>
2022-12-01 18:25:06 +0200

    MAT-8641: export/import localization bundles

--- java/net/anotheria/anosite/cms/action/LocalizationBundleImportMafAction.java
+++ java/net/anotheria/anosite/cms/action/LocalizationBundleImportMafAction.java
@@ -1,13 +1,11 @@
 package net.anotheria.anosite.cms.action;
 
 import net.anotheria.anosite.gen.shared.action.BaseToolsAction;
-import net.anotheria.anosite.gen.shared.service.AnositeLanguageUtils;
 import net.anotheria.maf.action.ActionCommand;
 import net.anotheria.maf.action.ActionMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.List;
 
 /**
  * @author asamoilich.
@@ -20,8 +18,7 @@ public class LocalizationBundleImportMafAction extends BaseToolsAction {
     }
 
     public ActionCommand anoDocExecute(ActionMapping aMapping, HttpServletRequest aReq, HttpServletResponse aRes) throws Exception {
-        List<String> languages = AnositeLanguageUtils.getSupportedLanguages();
-        aReq.setAttribute("languages", languages);
+        aReq.setAttribute("languages", getSupportedLanguages());
         aReq.setAttribute("selectedLanguage", "AT");
         return aMapping.success();
     }
